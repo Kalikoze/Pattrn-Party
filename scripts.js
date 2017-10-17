d8dee3eee253055514d7f70b2e3c5e01d7d349cb
Travis Rollins <TROLL59910@aol.com>
2017-10-17 15:23:52 -0600

    Refactor append and set it to variable

--- scripts.js
+++ scripts.js
@@ -4,15 +4,7 @@ const images = ['leopard', 'sealion', 'wolf', 'lemur'];
 const changeTab = tab => {
 	const tabNum = tab.innerText.slice(4, 5);
 	const animalList = [leopard, seaLion, manedWolf, lemur];
-	$('.is-active').removeClass('is-active');
-	$(tab).toggleClass('is-active');
-	$(window).width() > 980 ? appendDisplay(tabNum, animalList) : appendTab(tab, tabNum, animalList);
-};
-
-const appendTab = (tab, tabNum, animalList) => {
-	changeSign(tab)
-	$('.display').remove();
-	$('.is-active').after(`<section class="display">
+	const description = `
 		<img src="assets/${images[tabNum-1]}.jpg" alt="">
 		<div class="info">
 			<h3>${animals[tabNum-1]}</h3>
@@ -26,7 +18,19 @@ const appendTab = (tab, tabNum, animalList) => {
 					${animalList[tabNum-1][1] || ''}
 				</p>
 			</section>
-		</div>
+		</div>`;
+
+	$('.is-active').removeClass('is-active');
+	$(tab).toggleClass('is-active');
+	$(window).width() > 980 ? appendDisplay(tabNum, animalList, description) : appendTab(tab, tabNum, animalList, description);
+};
+
+const appendTab = (tab, tabNum, animalList, description) => {
+	changeSign(tab)
+	$('.display').remove();
+	$('.is-active').after(`
+	<section class="display">
+		${description}
 	</section>`)
 }
 
@@ -36,23 +40,9 @@ const changeSign = tab => {
 	value.text() === '+' ? value.text('-') : value.text('+')
 }
 
-const appendDisplay = (tabNum, animalList) => {
+const appendDisplay = (tabNum, animalList, description) => {
 	$('.display').empty()
-	$('.display').append(`
-		<img src="assets/${images[tabNum-1]}.jpg" alt="">
-		<div class="info">
-			<h3>${animals[tabNum-1]}</h3>
-			<hr class="display-hr">
-			<section class='description'>
-				<p>
-					${animalList[tabNum-1][0]}
-				</p>
-				<br>
-				<p>
-					${animalList[tabNum-1][1] || ''}
-				</p>
-			</section>
-		</div>`)
+	$('.display').append(`${description}`)
 }
 
 $('.tab').click(e => changeTab(e.currentTarget));
